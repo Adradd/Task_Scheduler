578c52af73921569f2b982de7b45f4b196982f06
Gavin McDaniel <me@gavinmcdaniel.com>
2026-02-19 16:15:03 -0500

    Basic react for testing

---
+++ Frontend/task-scheduler/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import logo from '../assets/logo.svg'
+
+function Navbar() {
+    return (
+        <header>
+            <img src={logo} alt={"schedule.me Logo"} />
+        </header>
+    )
+}
+
+export default Navbar
---
+++ Frontend/task-scheduler/src/components/TaskView.jsx
@@ -0,0 +1,27 @@
+
+
+
+
+
+function TaskView() {
+    return (
+        <>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Task</th>
+                        <th>Deadline</th>
+                        <th>Completion Time</th>
+                        <th>Priority</th>
+                        <th>Project</th>
+                        <th>Tags</th>
+                        <th>Subtasks</th>
+                        <th>Comments</th>
+                    </tr>
+                </tbody>
+            </table>
+        </>
+    )
+}
+
+export default TaskView
