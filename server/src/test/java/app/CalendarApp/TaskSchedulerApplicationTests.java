d7e7076095ad0b71f0f3757c53ed74f6ba4f843e
Gavin McDaniel <me@gavinmcdaniel.com>
2026-04-06 12:39:50 -0400

    Restructured project to live in respective packages client and server. pom.xml will need to be reselected in the right sidebar in IntelliJ for project to function. Make sure .env is moved into the server folder root as well. Target folder can be deleted.

--- server/src/test/java/app/CalendarApp/TaskSchedulerApplicationTests.java
+++ server/src/test/java/app/CalendarApp/TaskSchedulerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CapstoneTestApplicationTests {
+class TaskSchedulerApplicationTests {
 
     @Test
     void contextLoads() {
